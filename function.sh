19449ee1a3dd03dcfcaa71f8e31bc56c82cb1650
KiranPatil <kiran@gmail.com>
2026-02-16 11:57:59 +0000

    function added

---
+++ function.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+haldi(){
+echo " hadli chalu ho gayi"
+echo " photo shoot ho gaya"
+echo " function khatam ho gaya"
+
+}
+
+# mohit called haddli function
+haldi
+
+#sampat called haldi function
+haldi
